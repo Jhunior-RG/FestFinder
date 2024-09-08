6c13e9e0c8d642ca563ebb1c73268e8c52d8bffd
Byskull2000 <grajedadiego1@gmail.com>
2024-09-07 22:02:59 -0400

    Formulario de inicio de sesión y de registro (Solo frontend)

--- components/Login.tsx
+++ components/Login.tsx
@@ -1,23 +1,38 @@
-import { Text, TextInput } from "react-native";
+import { Text, TextInput, TouchableOpacity } from "react-native";
 import LoginGoogle from "./LoginGoogle";
+import Styles from "../globalStyles/styles";
 
 const Login = () => {
     return (
         <>
-            <Text >e-mail</Text>
+            <Text style={Styles.title} >Iniciar Sesion</Text>
             <TextInput
-                
-                placeholder="example@example"
+                placeholder="Email"
+                keyboardType="email-address"
+                placeholderTextColor="purple"
+                style={Styles.input}
             />
-            <Text>Password</Text>
             <TextInput
-                placeholder="your password"
+                placeholder="Contraseña"
                 secureTextEntry={true}
-                
+                placeholderTextColor="purple"
+                style={Styles.input}
             />
+            <TouchableOpacity
+                style={Styles.button}
+                onPress={() => {
+                    console.log("PRUEBA DE QUE FUNCIONA");
+                }}
+            >
+                <Text style={Styles.buttonText}>Iniciar Sesion</Text>
+            </TouchableOpacity>
+            <Text style = {[Styles.textDecoration, Styles.espaciado]}>Inicia sesion usando</Text>
             <LoginGoogle />
+
         </>
     );
 };
+
+
 
 export default Login;
--- globalStyles/styles.tsx
+++ globalStyles/styles.tsx
@@ -2,14 +2,54 @@ import { StyleSheet } from "react-native";
 
 const estilos = StyleSheet.create({
     title: {
+        color: 'purple',
+        fontWeight: '500',  
         fontSize: 26,
-        color: 'black',
+        marginBottom: 15
     },
     container: {
         flex: 1,
         justifyContent: 'center',
         alignItems: 'center',
+    },
+    input: {
+        borderColor: 'purple',  
+        borderWidth: 2,         
+        borderRadius: 10,       
+        padding: 10,            
+        marginBottom: 12,       
+        width: '80%',           
+    },
+    textDecoration: {
+        color: 'purple',
+        textAlign: 'left'
+    },
+    button: {
+        backgroundColor: 'purple',    
+        padding: 10,                  
+        borderRadius: 10,             
+        alignItems: 'center',         
+        marginTop: 20,               
+        width: '80%',                
+    },
+    buttonText: {
+        color: 'white',               
+        fontSize: 16,                          
+    },
+    linkContainer: {
+        flexDirection: 'row',    
+        alignItems: 'center', 
+        marginTop: 10,   
+    },
+    linkText: {
+        color: 'purple',         
+        fontWeight: '400',      
+    },
+    espaciado:{
+        marginTop: 20,
+        marginBottom: 10,
     }
+
 });
 
 export default estilos;
